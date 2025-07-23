1419b25cc1086124a0832ebd77d34f1ee1694272
yeonhee-cho <yhcho.proj@gmail.com>
2025-07-23 21:08:38 +0900

    [Add] 배열2

--- 01_java/07_array.java
+++ 01_java/07_array.java
@@ -1,6 +1,7 @@
 import java.sql.SQLOutput;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.OptionalInt;
 
 public class C07Array {
     public static void main(String[] args) {
@@ -139,7 +140,76 @@ public class C07Array {
             }
         }
         System.out.println(Arrays.toString(arr5));
-     }
+
+        // 검색 알고리즘 구현
+        // 배열에서 어떤 숫자 찾기
+        int[] arr6 = {3, 4, 7, 9, 7, 10};
+        // 방법1 for 문과 break 문을 이용한 직접 구현
+        int answer = 0;
+        for (int i = 0; i < arr6.length; i++) {
+            if(arr6[i] == 7){
+                answer = i;
+                break;
+            }
+        }
+        System.out.println(answer);
+
+        // 방법2 : binarySearch
+        // 배열이 오름차순 정렬되어 있어야 한다는게 전제
+        Arrays.sort(arr6);
+        System.out.println(Arrays.binarySearch(arr6,7));
+
+        // index 값을 찾는 알고리즘은 아니지만, 원하는 값이 해당 배열 안에 들어있는지 찾기 위한 방법
+        // 방법3 : StreamApi 와 Optional 객체를 활용
+        OptionalInt result = Arrays.stream(arr).filter(a -> a==7).findFirst(); // filter(a -> a==7): 7 하고 같은 애와 필터링
+        // Arrays.stream(arr) 복사본 만들기
+        if(result.isPresent()) {
+            System.out.println(result.getAsInt());
+            System.out.println("원하는 값이 존재합니다.");
+        }
+
+        // 배열의 요소 비교 : equals
+        // 값의 내용 뿐만 아니라 순서까지 동일해야 true가 나옴
+        int[] arr7 = {10,20,30};
+        int[] arr8 = {20,30,10};
+        // if 배열이 같은지 확인
+        System.out.println(Arrays.equals(arr7, arr8)); // false
+
+        // 배열 복사 : copyOf
+        int[] arr9 = {3,4,7,8,10};
+        int[] new_arr = Arrays.copyOf(arr, 4);
+
+        System.out.println(Arrays.toString(new_arr));
+
+        // 배열 복사 : copyOfRange 1부터 3미만
+        int[] new_arr9 = Arrays.copyOfRange(arr,1,3);
+        System.out.println(Arrays.toString((new_arr9)));
+
+        // 2차원 배열
+        int[][] arr10 = new int[2][3]; // {{10,20,30},{40,50,60}}
+        arr10[0][0] = 10;
+        arr10[0][1] = 20;
+        arr10[0][2] = 30;
+        arr10[1][0] = 40;
+        arr10[1][1] = 50;
+        arr10[1][2] = 60;
+        System.out.println(Arrays.deepToString(arr10)); // {{},{}}는 deep을 붙여줘야함
+
+
+        // 2차원 배열 내 각 요소를 평균값을 배열로 만들어 출력
+        int[] arr_new2 = new int[arr10.length];
+        for (int i = 0; i < arr10.length; i++) {
+            int total = 0;
+            for (int j = 0; j < arr10[i].length; j++) {
+                total += arr10[i][j];
+            }
+
+            arr_new2[i] = total/arr10[i].length;
+        }
+
+        // 가변 배열
+        int[][] arr12 = {{1,2,3},{10,20,30},{100,200,300,400},{1000,2000,3000,4000,5000}};
+    }
 }
 /*
 * 배열(Array)
@@ -156,21 +226,66 @@ public class C07Array {
 *       - 초기값이 있을 때
 *           - int[] odds = {1, 3, 5, 7, 9};
 *           - String[] weeks = {"월", "화", "수", "목"};
+*   - 배열의 특징
+*       - 배열의 길이는 고정
+*       - 동일한 데이터 타입
+*       - 인덱스에 의한 접근
+*
+*   - String[] weeks = {"월","화","수","목","금","토","일"};
+*   - 배열의 길이 : weeks.length
+*   - weeks[i]은 weeks 배열의 i번째 항목의 값을 의미
+*   - 이와 관련돼 자주 발생하는 오류
+*       - index에 접근 시 ArrayIndexOutOfBoundsException (배열에 숫자 잘 못 적었을 때)
+*           - weeks[7]
+*   - 배열의 index에 대한 접근은 for 문과 함께 많이 사용
+*       - for (int i = 0; i < weeks.length; i++){
+*           System.out.println(weeks[i]);
+*         }
+*   - 배열의 활용
+*       - 배열 합계와 평균 계산
+*       - 최대값과 최소값 찾기
+*       - 배열 뒤집기
+*       - 배열에서 k 번째로 큰 또는 작은 요소 찾기
+*           - Arrays의 sort 기능을 활용
+*   - Arrays 클래스
+*       - 배열을 다루기 위한 다양한 메소드가 포함
+*       - Arrays 클래스의 모든 메소드는 클래스 메소드(static method), new 필요 없음
+*   - 대표적인 메소드
+*       - 출력
+*           - Arrays.toString()
+*       - 정렬
+*           - 오름차순
+*               - Arrays.toString()
+*           - 내림차순
+*               - 오름차순 정렬 후 배열 뒤집기
+*               - Comparator 사용
+*                   - Arrays.sort(arr, Collections.reverseOrder());
+*                   - 해당 내용은 추후 lambda part에서 심화학습 예정
+*           - sort 알고리즘 직접 구현 해보기
+*               - 선택 정렬
+*               - 버블 정렬
+*   - 검색
+*       - for 문 + break를 통한 직접구현
+*       - 이진 검색
+*           - 이진 검색 알고리즘이란 가운데에 있는 항목을 키값과 비교하여 다음 탐색 위치를 결정
+*           - binarySearch()
+*           - sort() 메소드 등을 사용하여 미리 정렬되어 있어야만 이진 검색 제대로 동작
+*       - Stream API 활용
+*           - 추후 Stream API 파트에서 심화 학습
+*   - 비교
+*       - equals()
+*       - 두 배열의 모든 요소가 동일한지 확인(순서까지 일치해야함)
+*   - 배열 복사
+*       - copyOf(대상배열, endIndex)
+*           - 처음부터 endindex 까지
+*           - 대상 배열을 넘어서는 경우 초기값으로 세팅되어 endindex 만큼의 배열의 길이로 세팅
+*       - copyOfRange(대상배열, startIndex,endIndex)
+*   - 2차원 배열
+*       - 배열 안에 또 다른 배열이 있는 경우
+*   - 선언 방법 예시
+*       - int[][] arr = new int[2][3];
+*       - int[][] arr2 = new int[2][]; // 가변 배열, 첫 번째는 없이 쓸 수 없음
+*       - int[][] arr3 = {{1,2,3},{1,2,3}};
+*       - int[][] arr4 = {{10,20},{10,20,30},{10,20,30,40}};
 */
-/* 
-* 자료형 - 리스트(list)
-*   - java 에서 LIST는 여러 개의 요소를 저장하는 자료구조로서 *배열과 다르게 요소의 추가/삭제가 가능*
-*   - List는 인터페이스로서, ArrayList, LinkedList 등의 List 인터페이스를 구현한 자료형이 존재
-*       - 인터페이스가 무엇인지는 추후 인터페이스에서 학습 예정
-*       - 여기서는 ArrayList, LinkedList 등의 List 라는 큰 틀의 자료구조가 있고, 
-*         세부 카테고리로서 ArrayList, LinkedList 등이 있다는 정도만 학습
-* 
-*   - 기본 문법
-*       - ArrayList<String> pitches = new ArrayList<String>();
-*           - 일반적으로 뒷부분의 자료형은 생략
-*       - List<String> pitches = new ArrayList<>();
-*           - 가장 일반적인 선언방식으로 앞부분의 ArrayList 대신에 더 큰 카테고리인 List 자료구조를 선언하고 
-*             뒷부분에서 ArrayList를 선언. 
-*             그 이유와 의미에 대해선 추후 인터페이스에서 학습
-*           - ArrayList는 가장 많이 사용되는 List의 구현체
-*/
+
